c9c3c0afdd48ae09dd9bbd4a685ef6ee09f51377
Guishe <gui.bar.jim@gmail.com>
2020-12-22 19:05:52 -0300

    pwa running, ahora fixing que refreshee

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 window.onload = () => {
-  'use strict';
-  console.log('inicios');
-  
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
-}
+  "use strict";
+  console.log("inicios");
 
-document.getElementById('button').addEventListener('click', ()=>{
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => {
-    let powers = document.createElement('p')
-    powers.innerHTML = 'Powers Activated!'
-    document.getElementById('container').appendChild(powers)
-    console.log(json)
-  })
-})
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.update().then(() => {console.log('updated')});
+    });
+  }
+};
+
+document.getElementById("button").addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => {
+      let powers = document.createElement("p");
+      powers.innerHTML = "Powers Activated!";
+      document.getElementById("container").appendChild(powers);
+      console.log(json);
+    });
+});
