d4bc04339be38453f708f82baa7aa10256fc847a
Guishe <gui.bar.jim@gmail.com>
2020-12-23 13:38:40 -0300

    pwa update version 1.0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'funstuffv1.0.1';
+var cacheName = 'funstuffv1.0.2';
 var filesToCache = [
   '/',
   '/index.html',
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function(e) {
 });
 
 self.addEventListener('activate', (event) => {
-  var cacheKeeplist = ['funstuffv1.0.1'];
+  var cacheKeeplist = cacheName;
 
   event.waitUntil(
     caches.keys().then((keyList) => {
