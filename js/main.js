d08782f5c2370fab1c35eb382a46d8e084ae21a5
Guishe <gui.bar.jim@gmail.com>
2020-12-23 01:08:59 -0300

    pwa updating, ahora quiero boton de instalar

--- js/main.js
+++ js/main.js
@@ -5,17 +5,35 @@ window.onload = () => {
   if ("serviceWorker" in navigator) {
     console.log("navigator", navigator);
     console.log(navigator.serviceWorker);
-    navigator.serviceWorker.register("./sw.js")
-    .then(res=>{
-      console.log(res.scope)
-    })
-    .catch((error) => {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-   
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((res) => {
+        console.log(res.scope);
+      })
+      .catch((error) => {
+        // registration failed
+        console.log("Registration failed with " + error);
+      });
   }
 };
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  console.log(e);
+  
+  let install = document.createElement("button");
+  install.innerHTML = "Install!";
+  document.getElementById("container").appendChild(install);
+
+  e.prompt()
+  e.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
 
 document.getElementById("button").addEventListener("click", () => {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
