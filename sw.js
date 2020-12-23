1993b04bfde728ee258dabf0171a5b09c05f8871
Guishe <gui.bar.jim@gmail.com>
2020-12-23 13:13:37 -0300

    pwa install prompt on index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'v1';
+var cacheName = 'funstuffv1.0';
 var filesToCache = [
   '/',
   '/index.html',
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function(e) {
 });
 
 self.addEventListener('activate', (event) => {
-  var cacheKeeplist = ['v1'];
+  var cacheKeeplist = ['funstuffv1.0'];
 
   event.waitUntil(
     caches.keys().then((keyList) => {
